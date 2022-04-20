4f59fea77243b997ffbac0c13f5238f709213316
masacr3(leo R.) <leorhcp2030@gmail.com>
2022-04-19 22:10:47 -0300

    procesa salida standart

--- fixcol.c
+++ fixcol.c
@@ -1,5 +1,19 @@
+#define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main(int argc, char** argv){
+
+    char* linea = NULL;
+    size_t capacidad = 0;
+
+    for(int i=0; getline(&linea, &capacidad, stdin) > 0; i++){
+        char* linea_actual = linea;
+
+        fprintf(stdout, "%s\n", linea_actual);
+    }
+
+    free(linea);
+
     return 0;
 }
