f382a21589580360ae4d4f63737e6b269785a105
masacr3(leo R.) <leorhcp2030@gmail.com>
2022-04-20 22:32:04 -0300

    modularizando lineas #1

--- fixcol.c
+++ fixcol.c
@@ -3,8 +3,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#define ENTRADA_STANDART 2
+#define LECTURA_ARCHIVO 3
+#define TAM_BYTES 1
 
-size_t lenn(int n){
+size_t len_numero(int n){
     if (n == 0) return 1;
     size_t cont = n < 0 ? 1 : 0;
     n = n < 0 ? n*-1 : n;
@@ -17,30 +20,17 @@ size_t lenn(int n){
 }
 
 bool es_numero(char* s){
-    int a = atoi(s);
-    return a == 0 ? strcmp("0", s) == 0 : lenn(a) == strlen(s);
+    int numero = atoi(s);
+    return numero == 0 ? strcmp("0", s) == 0 : len_numero(numero) == strlen(s);
 }
 
-int main(int argc, char** argv){
+void checkea_parametros(int cant_argumentos, char** array_argumentos, bool *ok){
+    *ok = ( cant_argumentos == ENTRADA_STANDART || cant_argumentos == LECTURA_ARCHIVO || es_numero(array_argumentos[TAM_BYTES]) );
+    if (!*ok) fprintf(stderr, "%s\n","Error: Cantidad erronea de parametros.");
+}
 
-    //chequeo cantidad parametros
-    if ( !(argc == 2 || argc == 3 ) ){
-        fprintf(stderr, "%s\n","Error: Cantidad erronea de parametros.");
-        return 0;
-    }
-
-    //chequeo que el 2 parametro sea numero
-    if ( !es_numero(argv[1]) ){
-        fprintf(stderr, "%s\n","Error: Cantidad erronea de parametros.");
-        return 0;
-    }
-
-    int corte = atoi(argv[1]);
-    char* linea = NULL;
-    size_t capacidad = 0;
-
-    while(getline(&linea, &capacidad, stdin) > 0){
-        char* linea_actual = linea;
+void procesar_linea(char* linea, int corte){
+    char* linea_actual = linea;
         int k = 0;
         for(int i=0; linea_actual[i]; i++){
             if (k == corte){
@@ -50,6 +40,21 @@ int main(int argc, char** argv){
             fprintf(stdout, "%c", linea_actual[i]);
             k++;
         }
+}
+
+
+int main(int argc, char** argv){
+
+    bool ok;
+    checkea_parametros(argc, argv, &ok);
+    if (!ok) return 0;
+
+    int corte = atoi(argv[1]);
+    char* linea = NULL;
+    size_t capacidad = 0;
+
+    while(getline(&linea, &capacidad, stdin) > 0){
+        procesar_linea(linea, corte);
     }
 
     free(linea);
