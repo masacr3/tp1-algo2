12fca31fb7936bc4b9ac27a264f5012c49854ef5
masacr3(leo R.) <leorhcp2030@gmail.com>
2022-04-21 14:09:30 -0300

    se sacaron los defines de fixcol

--- fixcol.c
+++ fixcol.c
@@ -2,15 +2,11 @@
 #include <stdbool.h>
 #include "utils.h"
 #include "procesar_archivo.h"
-#define ENTRADA_STANDART 2
-#define LECTURA_ARCHIVO 3
-#define TAM_BYTES 1
 
 void fixcol(int cant_argumentos, char** argumentos ){
     bool ok;
     chequea_parametros(cant_argumentos, argumentos, &ok);
     if (ok) procesar_archivo(argumentos[TAM_BYTES], cant_argumentos == ENTRADA_STANDART ? NULL : argumentos[LECTURA_ARCHIVO], cant_argumentos == ENTRADA_STANDART);
-
 }
 
 int main(int argc, char** argv){
